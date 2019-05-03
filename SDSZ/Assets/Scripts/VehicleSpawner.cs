b3abf83f6a5ca3b5e17d3c9f4b635ca022d63290
Piotr Walat <pwalat98@gmail.com>
2019-05-03 21:05:27 +0200

    made two new prefabs (blue and red car) and vehicle spawner with clock direction changing and random car prefab (done for different car colors)
    turning is still the same on each car (right turn always) and simplestreetlight doesnt change from red to green second time!

--- SDSZ/Assets/Scripts/VehicleSpawner.cs
+++ SDSZ/Assets/Scripts/VehicleSpawner.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class VehicleSpawner : MonoBehaviour
 {
-    public GameObject vehicle;
-    Vector2 wheretoplace;
+    public GameObject vehicle_car_orange;
+    public GameObject vehicle_car_red;
+    public GameObject vehicle_car_blue;
+
     public float spawnRate = 4.0f;
+
     float nextSpawn = 4.0f;
+
     float XshiftLeft = -6.35f;
     float YshiftLeft = -0.39f;
-    int NumberOfVehicles = 4;
-    int VehicleCounter = 0;
 
+    float XshiftRight = +6.35f;
+    float YshiftRight = +0.39f;
+
+    float XshiftTop = -0.39f;
+    float YshiftTop = +3.6f;
+
+    float XshiftDown = +0.39f;
+    float YshiftDown = -3.6f;
+
+    Vector2 wheretoplace_top;
+    Vector2 wheretoplace_down;
+    Vector2 wheretoplace_left;
+    Vector2 wheretoplace_right;
+
+    int NumberOfVehicles = 6;
+    int i = 0;
 
 
     // Start is called before the first frame update
@@ -24,18 +42,98 @@ public class VehicleSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(VehicleCounter < NumberOfVehicles)
-        {
-            if (Time.time > nextSpawn)
+        
+        if (Time.time > nextSpawn && i<NumberOfVehicles)
             {
+                if (i % 4 == 1)
+                {
+                    wheretoplace_left = new Vector2(XshiftLeft, YshiftLeft);
+                    int colour_of_the_vehicle = Random.Range(1, 4);
+                    Debug.Log("pętla " + i + "\n Wylosowało wartosc:" + colour_of_the_vehicle);
+                    if (colour_of_the_vehicle == 1)
+                    {
+                        GameObject.Instantiate(vehicle_car_orange, wheretoplace_left, transform.rotation * Quaternion.Euler(0f, 0f, 270f));
+                    }
+                    if (colour_of_the_vehicle == 2)
+                    {
+                        GameObject.Instantiate(vehicle_car_red, wheretoplace_left, transform.rotation * Quaternion.Euler(0f, 0f, 270f));
+                    }
+                    if (colour_of_the_vehicle == 3)
+                    {
+                        GameObject.Instantiate(vehicle_car_blue, wheretoplace_left, transform.rotation * Quaternion.Euler(0f, 0f, 270f));
+                    }
+                    //break;
+                }
 
+
+
+                if (i % 4 == 2)
+                {
+                    wheretoplace_top = new Vector2(XshiftTop, YshiftTop);
+                    int colour_of_the_vehicle = Random.Range(1, 4);
+                    Debug.Log("pętla " + i + "\n Wylosowało wartosc:" + colour_of_the_vehicle);
+                    if (colour_of_the_vehicle == 1)
+                    {
+                        GameObject.Instantiate(vehicle_car_orange, wheretoplace_top, transform.rotation * Quaternion.Euler(0f, 0f, 180f));
+                    }
+                    if (colour_of_the_vehicle == 2)
+                    {
+                        GameObject.Instantiate(vehicle_car_red, wheretoplace_top, transform.rotation * Quaternion.Euler(0f, 0f, 180f));
+                    }
+                    if (colour_of_the_vehicle == 3)
+                    {
+                        GameObject.Instantiate(vehicle_car_blue, wheretoplace_top, transform.rotation * Quaternion.Euler(0f, 0f, 180f));
+                    }
+                    //break;
+                }
+
+
+
+                if (i % 4 == 3)
+                {
+                    wheretoplace_right = new Vector2(XshiftRight, YshiftRight);
+                    int colour_of_the_vehicle = Random.Range(1, 4);
+                    Debug.Log("pętla " + i + "\n Wylosowało wartosc:" + colour_of_the_vehicle);
+                    if (colour_of_the_vehicle == 1)
+                    {
+                        GameObject.Instantiate(vehicle_car_orange, wheretoplace_right, transform.rotation * Quaternion.Euler(0f, 0f, 90f));
+                    }
+                    if (colour_of_the_vehicle == 2)
+                    {
+                        GameObject.Instantiate(vehicle_car_red, wheretoplace_right, transform.rotation * Quaternion.Euler(0f, 0f, 90f));
+                    }
+                    if (colour_of_the_vehicle == 3)
+                    {
+                        GameObject.Instantiate(vehicle_car_blue, wheretoplace_right, transform.rotation * Quaternion.Euler(0f, 0f, 90f));
+                    }
+                    //break;
+                }
+
+
+
+                if (i % 4 == 0)
+                {
+                    wheretoplace_down = new Vector2(XshiftDown, YshiftDown);
+                    int colour_of_the_vehicle = Random.Range(1, 4);
+                    Debug.Log("pętla " + i + "\n Wylosowało wartosc:" + colour_of_the_vehicle);
+                    if (colour_of_the_vehicle == 1)
+                    {
+                        GameObject.Instantiate(vehicle_car_orange, wheretoplace_down, transform.rotation * Quaternion.Euler(0f, 0f, 0f));
+                    }
+                    if (colour_of_the_vehicle == 2)
+                    {
+                        GameObject.Instantiate(vehicle_car_red, wheretoplace_down, transform.rotation * Quaternion.Euler(0f, 0f, 0f));
+                    }
+                    if (colour_of_the_vehicle == 3)
+                    {
+                        GameObject.Instantiate(vehicle_car_blue, wheretoplace_down, transform.rotation * Quaternion.Euler(0f, 0f, 0f));
+                    }
+                    //break;
+                }
                 nextSpawn = Time.time + spawnRate;
-                wheretoplace = new Vector2(XshiftLeft, YshiftLeft);
-                //XshiftLeft += 1.0f;
-                GameObject.Instantiate(vehicle, wheretoplace, transform.rotation * Quaternion.Euler(0f, 0f, 270f));
-                VehicleCounter += 1;
+                i++;
             }
-        }
+        
         
     }
 }
