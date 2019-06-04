237c40950c311d78da300ef3daf45b26a65eb06c
Piotr Walat <pwalat98@gmail.com>
2019-06-04 22:58:41 +0200

    UI - done / both scenes
    exit button in simulation too

--- SDSZ/Assets/Scripts/Checker_Abilities.cs
+++ SDSZ/Assets/Scripts/Checker_Abilities.cs
@@ -6,13 +6,21 @@ using UnityEngine.UI;
 public class Checker_Abilities : MonoBehaviour
 {
     Slider car_slider;
-    
+    GameObject[] components;
+    GameObject value_of_cars;
     void Start()
     {
         car_slider = GameObject.Find("Car_Amount_slider").GetComponent<Slider>();
-        car_slider.transform.localPosition = new Vector3(-360, -245, 0);
+        car_slider.transform.localPosition = new Vector3(-800, -500, 0);
         car_slider.transform.localScale = new Vector3(0.8f, 0.8f, 0);
+        components = GameObject.FindGameObjectsWithTag("slider_component");
+        value_of_cars = GameObject.FindWithTag("slider_value");
         //car_slider.Value.disable;
+        foreach (GameObject comp in components)
+        {
+            comp.SetActive(false);
+        }
+        value_of_cars.transform.position += new Vector3(+90, +38, 0);
     }
 
     // Update is called once per frame
